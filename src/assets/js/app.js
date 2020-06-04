cc37c1de4f40c763c22e19f5b9d7667f22b5f401
danielmponda <dlmponda@gmail.com>
2020-06-04 23:29:46 +0200

    Access City's Coord using data from openWeatherApi and send it to darkSkyResults

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,7 @@
+import { method } from './methods'
+
+let theCityName, glat, glon
+
 /////////////////////////////////////////////////////////////////////////////////
 // 01 get location cords from the local machine
 window.addEventListener('load', () => {
@@ -13,6 +17,7 @@ window.addEventListener('load', () => {
   }
 })
 
+// 02 On window load Access lat & lon geolocation and fetch data from openWeatherApi and sent it to displayResults
 function openWeatherCoords(lat, lon) {
   fetch(
     `${method.openWeatherApi.baseurl}weather?lat=${lat}&lon=${lon}&appid=${method.openWeatherApi.key}`
@@ -24,7 +29,7 @@ function openWeatherCoords(lat, lon) {
     .then(displayResults)
 }
 
-// Get Input Search and Add keypress EventListener
+// 03 Get Input Search and Add keypress EventListener
 let searchBtn = null
 searchBtn = document.querySelector('.search-btn')
 searchBtn.addEventListener('click', searchGetResults)
@@ -41,4 +46,39 @@ function searchGetResults() {
         })
         .then(displayResults)
     : ''
+}
+
+// Get Input Search and Add keypress EventListener
+const searchinput = document.querySelector('input')
+searchinput.addEventListener('keypress', getResults)
+
+//  With the Results Send and API Request
+function getResults(e) {
+  let query
+  e.keyCode == 13 ? (query = searchinput.value) : ''
+  fetch(
+    `${method.openWeatherApi.baseurl}weather?q=${query}&units=metric&APPID=${method.openWeatherApi.key}`
+  )
+    .then(function (weather) {
+      return weather.json()
+    })
+    .then(displayResults)
+}
+
+// 04 Access City's Coord using data from openWeatherApi and send it to darkSkyResults
+function displayResults(city) {
+  const { lon, lat } = city.coord
+
+  searchinput.value = ''
+  glat = lat
+  glon = lon
+  theCityName = cityName[0].innerText = `${city.name}, ${city.sys.country}`
+  cityName[1].innerText = `${city.name}, ${city.sys.country}`
+  darkSkyResults(lat, lon)
+
+  try {
+    if (document.querySelector('.openlayer-map')) {
+      OpenlayerMap(lon, lat)
+    }
+  } catch (e) {}
 }
