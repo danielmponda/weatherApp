df0ba646c3e4ae88a223afd781a9a2cd7afcbaec
danielmponda <dlmponda@gmail.com>
2020-06-04 23:03:26 +0200

    On windowReload fetch data from openWeatherApi & sendto displayResults

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,7 +6,20 @@ window.addEventListener('load', () => {
     navigator.geolocation.getCurrentPosition((position) => {
       glat = position.coords.latitude
       glon = position.coords.longitude
+
+      openWeatherCoords(glat.toFixed(2), glon.toFixed(2))
     })
   } else {
   }
 })
+
+function openWeatherCoords(lat, lon) {
+  fetch(
+    `${method.openWeatherApi.baseurl}weather?lat=${lat}&lon=${lon}&appid=${method.openWeatherApi.key}`
+  )
+    // fetch(`${method.openWeatherApi.baseurl}weather?q=${query}&units=metric&APPID=${method.openWeatherApi.key}`)
+    .then(function (weather) {
+      return weather.json()
+    })
+    .then(displayResults)
+}
